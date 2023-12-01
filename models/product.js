e81fe1a684cb87aa63c149b232f3c0c4bea2fe35
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-12-01 14:56:26 +0530

    introduction of MVC structure of express js

---
+++ models/product.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsJSON = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+}
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+    
+    save() {
+        getProductsJSON(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll(cb) {
+        getProductsJSON(cb);
+    }
+
+}
