8453af46ec963115537e232ee9c75a521b176fd8
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-12-05 17:24:58 +0530

    Added a Sql database connection to the Project

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'S!ddh3sh'
+});
+
+module.exports = pool.promise();
